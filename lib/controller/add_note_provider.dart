73a88fd71e55dab9a4096b2382beb6d4ba52a6e2
Ziad Elsdaadany <ziadelsaadany73@gmail.com>
2023-01-29 07:43:30 +0200

    add note method

--- lib/controller/add_note_provider.dart
+++ lib/controller/add_note_provider.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/models/note_model.dart';
 
 class AddNoteProvider extends ChangeNotifier{
+  bool addLoading = false;
 addNote (NoteModel note )  {
-
+addLoading = true;
+notifyListeners() ;
+try {
+  var notesBox = Hive.box('notes_box');
+  notesBox.add(note);
+  addLoading= false;
+}catch(E) {
+  print (E);
+}
 }
 
 }
